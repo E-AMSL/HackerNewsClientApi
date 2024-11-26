68485c2edb0962c7176699fe40175d87e3fdec9c
Adam Szulak <TheCelofyz@gmail.com>
2024-11-26 11:14:18 +0100

    cache validation moved CachedItem class

--- HackerNewsClientApi/Models/CachedItem.cs
+++ HackerNewsClientApi/Models/CachedItem.cs
@@ -1,7 +1,17 @@
-﻿namespace HackerNewsClient.Api.Models;
+﻿using HackerNewsClient.Api.Services;
+using Microsoft.Extensions.Logging;
+
+namespace HackerNewsClient.Api.Models;
 
 internal class CachedItem<T>
 {
+    private readonly TimeSpan cacheLifetime = TimeSpan.FromMinutes(5);
+
     public T Item { get; set; }
     public DateTimeOffset CachedTime { get; set; }
+    public bool IsCacheValid()
+    {
+        return Item is not null 
+            && CachedTime + cacheLifetime > DateTimeOffset.UtcNow;
+    }
 }
