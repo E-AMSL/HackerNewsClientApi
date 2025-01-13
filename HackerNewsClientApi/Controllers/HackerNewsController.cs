5be2afd5d98f6c460e6f492b2caec52977ca8a2f
Adam Szulak <adam.szulak@gft.com>
2025-01-13 09:23:04 +0100

    limited request to 200 items and added retrieval or partial result form cache

--- HackerNewsClientApi/Controllers/HackerNewsController.cs
+++ HackerNewsClientApi/Controllers/HackerNewsController.cs
@@ -10,6 +10,7 @@ public class HackerNewsController : Controller
 {
     private readonly ILogger<HackerNewsController> logger;
     private readonly IHackerNewsService hackerNewsService;
+    private readonly int maxAmount = 200; //max returned by https://hacker-news.firebaseio.com/v0/beststories.json
 
     public HackerNewsController(ILogger<HackerNewsController> logger, IHackerNewsService hackerNewsService)
     {
@@ -20,6 +21,11 @@ public class HackerNewsController : Controller
     [HttpGet("{amount}")]
     public async Task<ActionResult<IEnumerable<HackerNewsStory>>> Get(int amount)
     {
+        if (amount > maxAmount)
+        {
+            return BadRequest("Can't request for more than 200 items");
+        }
+
         logger.LogInformation("GET - HackerNews - Get best stories from HackerNews API");
         ServiceResponse<IAsyncEnumerable<HackerNewsStory>> getNewsResult = await hackerNewsService.GetHackerNewsStoriesAsync(amount);
 
